b533ca4896aa301547116225788f0269c464c4d7
poxx1 <julianlastrama@outlook.com>
2022-11-23 00:59:04 -0300

    Digitoverifica cuando updateo un usuario

--- View/frmActualizarUsuario.Designer.cs
+++ View/frmActualizarUsuario.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmActualizarUsuario));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,6 +51,16 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Actualizar usuario";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Brutalismus 3000 - London - 2021";
             // 
             // frmActualizarUsuario
             // 
@@ -58,12 +69,15 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(783, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "frmActualizarUsuario";
             this.Text = "frmActualizarUsuario";
+            this.Load += new System.EventHandler(this.frmActualizarUsuario_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,5 +85,6 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- View/frmActualizarUsuario.cs
+++ View/frmActualizarUsuario.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Business;
+using DataAccess;
+using Models.DBModel;
 
 namespace View
 {
@@ -15,6 +18,48 @@ namespace View
         public frmActualizarUsuario()
         {
             InitializeComponent();
+        }
+
+        private void frmActualizarUsuario_Load(object sender, EventArgs e)
+        {
+            UserRepository ur = new UserRepository();
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = ur.ListDBUsers();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult response = MessageBox.Show("Esta seguro que desea restaurar el usuario?", "Restaurar", MessageBoxButtons.OKCancel);
+            if (response == DialogResult.OK)
+            {
+                try
+                {
+                    var user = (DBUsers)dataGridView1.CurrentRow.DataBoundItem;
+                    UserRepository ur = new UserRepository();
+                    ur.updateUser(user);
+                    //Updateo el digitoVerificador en la tabla de usuarios y en la tabla de horizontal y vertical.
+                    label1.Text = "Se updateo el usuario";
+
+                    DigitoVerificadorModel dv = new DigitoVerificadorModel();
+                    DigitoVerificadorService dg = new DigitoVerificadorService();
+                    DigitoVerificadorRepository dr = new DigitoVerificadorRepository();
+
+                    dv.digitoVerificador = dg.DigitoVerificarUsuario(user);
+                    dv.id_dv = user.id_dv.ToString();
+                    //dv.digitoVerificador = user.digitoVerificador;
+
+                    dg.UpdateDigitoVerificadorHorizontalUsuario(dv);
+                    dr.UpdateHorizontalUsuario(dv);
+                    dg.UpdateDigitoVerificadorVerticalUsuario();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+            else label1.Text = "No se updateo el usuario";
         }
     }
 }
