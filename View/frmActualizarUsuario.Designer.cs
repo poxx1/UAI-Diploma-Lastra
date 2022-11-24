645b49479dc246625c7f7b84680bca78bd820046
poxx1 <julianlastrama@outlook.com>
2022-11-24 06:51:38 -0300

    commit pelotudo

--- View/frmActualizarUsuario.Designer.cs
+++ View/frmActualizarUsuario.Designer.cs
@@ -45,6 +45,7 @@
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(696, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 39);
--- View/frmMain.designer.cs
+++ View/frmMain.designer.cs
@@ -296,7 +296,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(5, 390);
+            this.label2.Location = new System.Drawing.Point(476, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(144, 13);
             this.label2.TabIndex = 6;
